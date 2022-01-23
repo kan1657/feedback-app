f1c1537ca035f074bd17f7f3a5aeca842e038860
kan1657 <kan1657@hotmail.com>
2022-01-24 02:00:27 +0700

    Finish section4: Form, Validation, simple animation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import { v4 as uuuidv4 } from 'uuid'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -11,11 +13,16 @@ function App() {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
